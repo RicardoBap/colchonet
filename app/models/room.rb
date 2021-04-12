4970b12261a93d410b31ae716e535c832949c64d
RicardoBap <ric_bap@hotmail.com>
2021-04-12 22:28:23 +0000

    Adicionando validacao de preenchimento obrigatorio no 'model' de room

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,8 @@
 class Room < ApplicationRecord
     # attr_accessible :description, :location, :title
+  
+  validates_presence_of :title, :location, :description
+  validates_length_of :description, :minimum => 30, :allow_blank => false
 
   def complete_name
     "#{title} <> #{location}"
