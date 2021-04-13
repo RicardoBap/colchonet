c982e6f1862400d5d15ea8f23d2c8d626987617a
RicardoBap <ric_bap@hotmail.com>
2021-04-13 21:07:03 +0000

    Metodo pra gerar token implementado no model/user.rb e utilizando callbacks before_create

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,11 @@ class User < ApplicationRecord
   validates_uniqueness_of :email
 
   has_secure_password
+
+  before_create :generate_token
+
+  def generate_token
+    self.confirmation_token = SecureRandom.urlsafe_base64
+  end
+
 end
